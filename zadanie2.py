a6dc6c1c4c701873fedb0e9d238d7f6cd768d5cd
GloryShovel <sk.kamil6@wp.pl>
2020-11-14 18:36:55 +0100

    poprawka w dokumentacji

--- zadanie2.py
+++ zadanie2.py
@@ -6,4 +6,9 @@ def reliability_after_hours(hours, reliability):
 
 
 def exercise():
+    '''An industrial machine compresses natural gas into an interstate gas pipeline.
+    The compressor is on line 24 hours a day. (If the machine is down, a gas field has to be shut
+    down until the natural gas can be compressed, so down time is very expensive.)
+    The vendor knows that the compressor has a constant failure rate of 0.000001 failures/hr.
+    What is the operational reliability after 2500 hours of continuous service?'''
     print("Po 2500 godzinach reliability jest rowne: ", reliability_after_hours(2500, 0.000001))
--- zadanie3.py
+++ zadanie3.py
@@ -6,4 +6,7 @@ def failure_rate(time, reliability):
 
 
 def exercise():
+    '''What is the highest failure rate for a product if it is to have a reliability
+    (or probability of survival) of 98 percent at 5000 hours?
+    Assume that the time to failure follows an exponential distribution.'''
     print("Najwiekszy failure rate jest rowny: ", failure_rate(5000, 0.98))
